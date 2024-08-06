5e34e13f7c35ecf7411eed14ee6fc85c12359636
Fred Reimer <freimer@presidio.com>
2024-08-06 17:41:45 +0000

    formatting changes

--- main.go
+++ main.go
@@ -169,9 +169,9 @@ func handleObject() SectionHandler {
 		pathCommentAlreadyAdded := false
 		for _, m := range childSection.Value.(*jwcc.Object).Members {
 			newMember := &jwcc.Member{Key: m.Key, Value: m.Value}
-            newMember.Comments().Before = m.Comments().Before
-            newMember.Comments().Line = m.Comments().Line
-            newMember.Comments().End = m.Comments().End
+			newMember.Comments().Before = m.Comments().Before
+			newMember.Comments().Line = m.Comments().Line
+			newMember.Comments().End = m.Comments().End
 			newObj.Members = append(newObj.Members, newMember)
 
 			if !pathCommentAlreadyAdded {
@@ -207,10 +207,10 @@ func handleAutoApprovers() SectionHandler {
 func upsertMember[V *jwcc.Object | *jwcc.Array](doc *jwcc.Object, key string, val V) {
 	keyAst := ast.String(key)
 	index := doc.IndexKey(ast.TextEqual(key))
-    newMember := &jwcc.Member{Key: keyAst.Quote(), Value: jwcc.Value(val)}
-    newMember.Comments().Before = jwcc.Value(val).Comments().Before
-    newMember.Comments().Line = jwcc.Value(val).Comments().Line
-    newMember.Comments().End = jwcc.Value(val).Comments().End
+	newMember := &jwcc.Member{Key: keyAst.Quote(), Value: jwcc.Value(val)}
+	newMember.Comments().Before = jwcc.Value(val).Comments().Before
+	newMember.Comments().Line = jwcc.Value(val).Comments().Line
+	newMember.Comments().End = jwcc.Value(val).Comments().End
 	if index != -1 {
 		doc.Members[index] = newMember
 	} else {
